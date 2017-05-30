3d2b99a1ec0574cfc88574b6b7211f97ae6dcd9e
Edson Ticona <xcr33d@gmail.com>
2017-05-30 01:31:17 -0300

    Added comments

--- CPIO/2017/Estaciones-Espaciales/solucion.cpp
+++ CPIO/2017/Estaciones-Espaciales/solucion.cpp
@@ -1,3 +1,9 @@
+/**
+ * C. Estaciones espaciales
+ * https://omegaup.com/arena/problem/Estaciones-Espaciales#problems
+ * Si representamos una estacion como un vertice de un grafo, y los tuneles que las conectan como una 
+ * arista del grafo, entonces necesitamos encontrar el tamano de la covertura por vertices.
+ */
 #include <iostream>
 #include <vector>
 
@@ -6,21 +12,21 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    vector<vector<int>> G(n);
+    vector<vector<int>> G(n);	//Matriz con los u_i
     for (int i = 1; i < n; ++i) {
         int x;
         cin >> x;
         G[x - 1].push_back(i);
     }
-    vector<vector<int>> DP(n, vector<int>(2));
-    for (int u = n - 1; u >= 0; --u) {
+    vector<vector<int>> DP(n, vector<int>(2));	//tabla con el numero de agentes requeridos para cada arbol
+    for (int u = n - 1; u >= 0; --u) {		//bottom-up 
         int taken[] = {0, 0};
         for (int v : G[u]) {
             taken[0] += DP[v][0];
             taken[1] += DP[v][1];
         }
-        DP[u][0] = 1 + taken[1];
-        DP[u][1] = min(1 + taken[1], taken[0]);
+        DP[u][0] = 1 + taken[1];		
+        DP[u][1] = min(1 + taken[1], taken[0]);	//numero de agentes requeridos
     }
     cout << DP[0][1] << endl;
 }

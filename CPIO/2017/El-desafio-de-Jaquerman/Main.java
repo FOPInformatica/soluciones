28ddd434728b63728626930b2d0c04e18dcc989e
Aldo Culquicondor <alculquicondor@gmail.com>
2017-05-29 19:55:57 -0500

    El desafío de Jaquerman

--- CPIO/2017/El-desafio-de-Jaquerman/Main.java
+++ CPIO/2017/El-desafio-de-Jaquerman/Main.java
@@ -1,12 +1,18 @@
+/*
+ * https://omegaup.com/arena/problem/El-desafio-de-Jaquerman/
+ * 
+ * Se observa que para i<n el mcd(i, i+1, ..., n) es siempre 1 y
+ * para i=n es n.
+ * Por tanto, la sumatoria queda 1+1+...+1+n, donde
+ * la cantidad de 1s es n-1.
+ */
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         int n = (new Scanner(System.in)).nextInt();
-        int ans = 0;
-        for (int i = 0; i < n - 1; ++i)
-            ++ans;
-        ans += n;
-        System.out.println(ans);
+        long rpta = (n - 1) + n;  // Equivalentemente: 2 * n - 1
+        System.out.println(rpta);
     }
 }

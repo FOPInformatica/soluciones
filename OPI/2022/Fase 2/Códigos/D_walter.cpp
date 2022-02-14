9f00d588df03eac4aad70a854e46a6438eca0d4d
Victor Racso Galvan Oyola <racso.galvan@deeppit.org>
2022-02-14 13:40:57 -0500

    Agregango solucionario OPI 2022 Fase 2

--- OPI/2022/Fase 2/Códigos/D_walter.cpp
+++ OPI/2022/Fase 2/Códigos/D_walter.cpp
@@ -7,7 +7,7 @@ using namespace std;
 #define N 705
 typedef long long ll;
 
-int mod = 1e9 + 7;
+const ll mod = 1e9 + 7;
 
 ll DP[N][N];
 ll C[N][N];
@@ -15,19 +15,39 @@ ll C[N][N];
 inline ll fix(ll x) {
     return (x % mod + mod) % mod;
 }
+/*
+ll f(int n, int h) {
+    if (n == 0) return 1;
+    if (h == 0) return 0;
+    ll &res = DP[n][h];
+    if (res != -1) 
+        return res;
+    res = 0;
+    for (int i = 1; i <= n; i++) {
+        res += fix(C[n - 1][i - 1] * fix(f(i - 1, h - 1) * f(n - i, h - 1)));
+    }
+    res = fix(res);
+    return res;
+}*/
 
 int main() {
     int n;
     cin >> n;
+    //memset(DP, -1, sizeof DP);
+    memset(C, 0, sizeof C);
 
     for (int i = 0; i < N; i++)
     for (int j = 0; j <= i; j++) {
         if (i == 0 || j == 0) C[i][j] = 1;
         else C[i][j] = (C[i - 1][j] + C[i - 1][j - 1]) % mod;
     }
-    DP[0][0] = 1;
-    for (int h = 1; h <= n; h++) {
-	DP[0][h] = 1;
+    for (int h = 0; h <= n; h++) {
+        DP[0][h] = 1;
+        if (h == 0) {
+            for (int nn = 1; nn <= n; nn++)
+                DP[nn][h] = 0;
+            continue;
+        }
         for (int nn = 1; nn <= n; nn++){
             ll &res = DP[nn][h];
             res = 0;
@@ -39,8 +59,8 @@ int main() {
 
     ll res = 0;
     for (int h = 1; h <= n; h++) {
-        res += (DP[n][h] + mod - DP[n][h - 1]) * (h - 1);
+        res += fix(DP[n][h] - DP[n][h - 1]) * (h - 1);
+        res = fix(res);
     }
-    cout << fix(res) << endl;
-    return 0;
+    cout << res << endl;
 }
